42763f429fe1e051b760e6321b8ba1e4523634d2
JohnRain55 <77028227+JohnRain55@users.noreply.github.com>
2021-04-15 15:29:39 +0300

    Added transition from the main to the login page

--- test_main_page.py
+++ test_main_page.py
@@ -3,24 +3,30 @@ from pages.login_page import LoginPage
 
 
 def test_quest_should_see_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_url()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
 
 
 def test_quest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_form()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_form()
 
 
 def test_should_be_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_register_form()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_register_form()
 
 
 def test_guest_can_go_to_login_page(browser):
