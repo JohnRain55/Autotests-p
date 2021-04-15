8e1ebff816c1ee564e741dda6b82d52522d5635b
JohnRain55 <77028227+JohnRain55@users.noreply.github.com>
2021-04-15 20:55:37 +0300

    Added time.sleep(2) to pass the tests correctly

--- test_product_page.py
+++ test_product_page.py
@@ -1,15 +1,16 @@
 from pages.product_page import ProductPage
+import time
 
 
 def test_quest_should_see_add_to_basket_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_button()
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
@@ -17,17 +18,20 @@ def test_guest_can_add_product_to_basket(browser):
 
 
 def test_quest_should_see_message_about_adding_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
+    time.sleep(2)
     page.should_be_book_name()
 
+
 def test_quest_should_see_message_about_cost_of_the_book_in_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
+    time.sleep(2)
     page.should_be_book_cost()
